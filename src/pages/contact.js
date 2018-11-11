a9b4a3edd4411a299c7d3f8a3b2073ccae86ce8c
Keegan Gladstone <keegan_gladstone@datahouse.com>
2018-11-10 16:29:06 -1000

    changed form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import styled from 'styled-components'
+import { Row, Col, Form, FormGroup, Label, Input, Button } from 'reactstrap'
 
 import Layout from '../components/layout'
 
@@ -17,43 +18,44 @@ const Container = styled('div')`
 `
 
 const Block = styled('div')`
+    display: flex;
+    flex-direction: column;
   margin: 0 auto;
   max-width: 800px;
-  padding: 20px;
-  text-align: justify;
-`
-
-const FormRow = styled('div')`
-  display: flex;
-  flex-direction: row;
-  align-items: baseline;
-  justify-content: start;
-`
-
-const Form = styled('form')`
   width: 100%;
-  min-width: 400px;
-  margin: 20px auto;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  padding: 20px;
 `
-const FormInput = styled('input')`
-  margin-left: 20px;
-  width: 250px;
-`
-const FormTextArea = styled('textarea')`
-  margin-left: 20px;
-  width: 250px;
-`
+
+// const FormRow = styled('div')`
+//   display: flex;
+//   flex-direction: row;
+//   align-items: baseline;
+//   justify-content: start;
+// `
+
+// const Form = styled('form')`
+//   width: 100%;
+//   min-width: 400px;
+//   margin: 20px auto;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+// `
+// const FormInput = styled('input')`
+//   margin-left: 20px;
+//   width: 250px;
+// `
+// const FormTextArea = styled('textarea')`
+//   margin-left: 20px;
+//   width: 250px;
+// `
 
 const ContactPage = () => (
   <Layout>
     <Container>
       <Block>
         <h3 style={{textAlign: "center"}}>Contact me today to see how I can help.</h3>
-
-        <Form
+        {/* <Form
           name="contact"
           method="POST"
           data-netlify="true"
@@ -75,9 +77,35 @@ const ContactPage = () => (
           <FormRow>
             <button type="submit">Send</button>
           </FormRow>
-        </Form>
+        </Form> */}
 
-        <img src={MessengerDoodle} width="80%" />
+        <Form name="contact"
+          method="POST"
+          data-netlify="true"
+          action="/success" className="form">
+            <Col>
+                <FormGroup>
+                    <Label>Your Name:</Label>
+                    <Input type="text" name="name"/>
+                </FormGroup>
+            </Col>
+            <Col>
+                <FormGroup>
+                    <Label>Your Email:</Label>
+                    <Input type="email" name="email"/>
+                </FormGroup>
+            </Col>
+            <Col>
+                <FormGroup>
+                    <Label>Message:</Label>
+                    <Input type="textarea" name="message"/>
+                </FormGroup>
+            </Col>
+            <Col>
+            <Button type="submit">Send it off!</Button>
+            </Col>
+        </Form>
+        <img src={MessengerDoodle} width="80%" style={{margin: "0 auto"}} />
       </Block>
     </Container>
   </Layout>
