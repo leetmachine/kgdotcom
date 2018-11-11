36eb9928e842a527f7798dbb772bd666b66e9ff2
Keegan Gladstone <keegan_gladstone@datahouse.com>
2018-11-10 16:29:35 -1000

    cleaned up code

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -26,59 +26,11 @@ const Block = styled('div')`
   padding: 20px;
 `
 
-// const FormRow = styled('div')`
-//   display: flex;
-//   flex-direction: row;
-//   align-items: baseline;
-//   justify-content: start;
-// `
-
-// const Form = styled('form')`
-//   width: 100%;
-//   min-width: 400px;
-//   margin: 20px auto;
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-// `
-// const FormInput = styled('input')`
-//   margin-left: 20px;
-//   width: 250px;
-// `
-// const FormTextArea = styled('textarea')`
-//   margin-left: 20px;
-//   width: 250px;
-// `
-
 const ContactPage = () => (
   <Layout>
     <Container>
       <Block>
-        <h3 style={{textAlign: "center"}}>Contact me today to see how I can help.</h3>
-        {/* <Form
-          name="contact"
-          method="POST"
-          data-netlify="true"
-          action="/success"
-        >
-          <input type="hidden" name="form-name" value="contact" />
-          <FormRow>
-            <p>Your Name:</p>
-            <FormInput type="text" name="name" />
-          </FormRow>
-          <FormRow>
-            <p>Your Email:</p>
-            <FormInput type="email" name="email" />
-          </FormRow>
-          <FormRow>
-              <p>Message:</p>
-              <FormTextArea name="message"/>
-          </FormRow>
-          <FormRow>
-            <button type="submit">Send</button>
-          </FormRow>
-        </Form> */}
-
+        <h3 style={{textAlign: "center", marginBottom: "20px"}}>Contact me today to see how I can help.</h3>
         <Form name="contact"
           method="POST"
           data-netlify="true"
